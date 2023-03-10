c36a1e64673f7f9026fbc8e999e336f59b3ccb86
gaejangsu <teplotaxl@icloud.com>
2023-03-10 21:11:37 +0900

    adfadfadf

--- Boolean.java
+++ Boolean.java
@@ -2,6 +2,6 @@ class Boolean {
 	static String getBoolean(String args) {
 		if(args.equals("false")) { return "[00000000]"; }
 		else if(args.equals("true")) { return "[00000001]"; }
-		return "";
+		return "";eee
 	}
 }
